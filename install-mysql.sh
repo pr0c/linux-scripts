0a8236784817a81d935e183eafbfbed336e033cf
Vladyslav <vlad20079712@gmail.com>
2019-03-15 15:37:19 +0200

    Added installation script

---
+++ install-mysql.sh
@@ -0,0 +1,3 @@
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+sudo apt -y install mysql-server
