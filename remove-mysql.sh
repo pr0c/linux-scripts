bd1d796a2e62c7666f954287fa2fda4aa880f488
Vladyslav <vlad20079712@gmail.com>
2019-03-15 15:29:28 +0200

    Updated remove-mysql script

--- remove-mysql.sh
+++ remove-mysql.sh
@@ -1,4 +1,5 @@
 sudo apt-get purge mysql-server mysql-client mysql-common mysql-server-core-* mysql-client-core-*
+sudo apt purge mysql-server mysql-server-5.7 mysql-server-core-5.7
 sudo rm -rf /etc/mysql /var/lib/mysql
 sudo apt-get autoremove
 sudo apt-get autoclean
